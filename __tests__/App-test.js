40511a345c77a4cb1da29928ff08e7b8881104ef
Uladzislau Arlouski <uarlouski@gmail.com>
2020-07-22 13:20:48 +0300

    Add functionality to check taps

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -9,6 +9,14 @@ import App from '../src/App';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
+jest.useFakeTimers();
+jest.mock('@react-navigation/drawer', () => ({
+  createDrawerNavigator: jest.fn().mockReturnValue({
+    Navigator: ({children}) => <>{children}</>,
+    Screen: ({children}) => <>{children}</>,
+  }),
+}));
+
 it('renders correctly', () => {
   renderer.create(<App />);
 });
