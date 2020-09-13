0a2da27519e32579d94167300072b3f113d54da4
Uladzislau Arlouski <30923336+uarlouski@users.noreply.github.com>
2020-09-13 13:03:05 +0300

    Add functionality to check waits (#13)

---
+++ src/sheet/index.js
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  icon: {
+    color: '#464547',
+  },
+  centredView: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+module.exports = {
+  globalStyles: styles,
+};
